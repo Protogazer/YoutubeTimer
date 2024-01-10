aa7b161da88f217f1c348927ea924d53cf503bf2
Protogazer <alexfmjohnson@gmail.com>
2024-01-10 14:29:54 -0700

    first commit

---
+++ scripts/blocker.js
@@ -0,0 +1,4 @@
+/**
+ * blocks youtube page from displaying or interaction.
+ * Doesn't need to be fort knox, just enough to get me to think
+ */
---
+++ scripts/timer.js
@@ -0,0 +1,7 @@
+/**
+ * TODO
+ * Listen with active tab for youtube.com url. 
+ * Decrement a timer from a set max
+ * once timer is <= 0, block further redirects from the page, and show a popup
+ * allow for an extension, but require a break first
+ */
