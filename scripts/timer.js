b6fc68c1a3ce4a766a03c1793bafbc99fb8bad0c
Protogazer <alexfmjohnson@gmail.com>
2024-01-11 22:14:07 -0700

    timer logic, next the blocker

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,7 +1,41 @@
+// this script will run when on youtube's domain
 /**
  * TODO
- * Listen with active tab for youtube.com url. 
- * Decrement a timer from a set max
  * once timer is <= 0, block further redirects from the page, and show a popup
  * allow for an extension, but require a break first
  */
+
+// set default timer in minutes allow user to change
+var minutes = 180;
+
+// milliseconds per minute
+const milliseconds = 60000;
+
+// timer decrement interval
+const interval = 5000;
+
+// setting timer for decrementation
+var timeSpent = (minutes * milliseconds)
+
+
+// starts the countdown
+function countdown(minutes){
+    // check if an interval has been setup already
+    if (!nIntervId) {
+        nIntervId = setInterval(decrement(interval), interval);
+    }
+};
+
+
+// begins decrementing from the timer, checks if timer has reached or surpassed 0.
+function decrement(interval_ms) {
+    timeSpent = timeSpent-interval_ms;
+    if (timeSpent <= 0){
+        clearInterval(nIntervId)
+        blockPage();
+    }
+}
+
+function blockPage() {
+    // block the page somehow... to be determined.
+}
