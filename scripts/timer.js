6fb7a10b2e384909c5da0d0c0ec32d3c354e9d5c
protogazer <alexfmjohnson@gmail.com>
2024-01-18 21:02:20 -0700

    time to refactor this whole thing. it sucks

--- scripts/timer.js
+++ scripts/timer.js
@@ -15,20 +15,23 @@ const milliseconds = 60000;
 // frequency the script updates at in ms
 const interval = 5000;
 
+var nIntervId;
+
 // create a timer object with time remaining AND date established
 var day = new Date();
 var timerInfo = {timeRemaining:(minutes * milliseconds), date:day.getDate()}
 
 // save timer to local storage along with date initiated if not set already
 if (! localStorage.getItem("timerInfo")) {
-    localStorage.setItem("timerInfo", timerInfo);
+    localStorage.setItem("timerInfo", JSON.stringify(timerInfo));
 };
 
 
 // starts the countdown
 function countdown(){
     // check if an interval has been setup already
-    if (!nIntervId) {
+    if (nIntervId === undefined) {
+        console.log("creating Interval")
         nIntervId = setInterval(decrement(interval), interval);
     }
     else {
@@ -39,14 +42,15 @@ function countdown(){
 
 // begins decrementing from the timer, checks if timer has reached or surpassed 0.
 function decrement(interval_ms) {
-    let timer = dateCheck(localStorage.getItem("timerInfo"));
+    console.log(localStorage.getItem("timerInfo"));
+    let timer = dateChecker(localStorage.getItem("timerInfo"));
 
     timer.timeRemaining = timer.timeRemaining - interval_ms;
-    localStorage.setItem = ("timerInfo", timer);
+    localStorage.setItem = ("timerInfo", JSON.stringify(timer));
 
-    console.log("Time Remaining: ", timeCounter);
+    console.log("Time Remaining: ", localStorage.getItem("timerInfo"));
 
-    if (timeCounter <= 0){
+    if (timer.timeRemaining <= 0){
         clearInterval(nIntervId)
         blockPage();
     }
@@ -56,12 +60,19 @@ function decrement(interval_ms) {
 // checks for date rollover (and resets if found) then returns the timer localStorage item
 function dateChecker(timerObject) {
     let currentDate = new Date();
+    var date;
+    if (timerObject !== undefined) {
+        var parsedObj = JSON.parse(timerObject);
+        date = parsedObj.date;
+    }
 
-    if (timerObject.date != currentDate.getDate()) { //this could bite me in the ass if it has been a month or more since the last date update
+    if (date != currentDate.getDate()) { //this could bite me in the ass if it has been a month or more since the last date update
         console.log("It's a new day!");
         // make a new timer entry and reset the time remaining
-        let newTimerInfo = {timeRemaining:(minutes * milliseconds), date: dateCheck.getDate()};
-        localStorage.setItem("timerInfo", newTimerInfo);
+        console.log("Time remaining: ", (minutes * milliseconds));
+        let newTimerInfo = {timeRemaining:(minutes * milliseconds), date: currentDate.getDate()};
+        localStorage.setItem("timerInfo", JSON.stringify(newTimerInfo));
+        console.log("time local storage remaining: ", localStorage.getItem("timerInfo"))
     }
 
     return localStorage.getItem("timerInfo");
