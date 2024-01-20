4c02574937d3fe549ad7be702888238e64f3e7bf
protogazer <alexfmjohnson@gmail.com>
2024-01-19 23:16:48 -0700

    Found fix for Content Security Policy blocking setInterval

---
+++ scripts/repeater.js
@@ -0,0 +1,26 @@
+var clock = 60000;
+var timeInterval = 5000; // 5 seconds
+
+function countdown(timeInterval) {
+    clock = clock - timeInterval;
+    if (clock <= 0 ) {
+        console.log("BOOM!");
+        clearInterval(intervalId);
+    }
+    else {
+        console.log("Time remaining: ", clock);
+    }
+}
+
+// the setInterval function does not work with CSP evals because it can execute strings
+// because of this, the countdown must be written inline
+let intervalId = setInterval(function () {
+    clock = clock - timeInterval;
+    if (clock <= 0 ) {
+        console.log("BOOM!");
+        clearInterval(intervalId);
+    }
+    else {
+        console.log("Time remaining: ", clock);
+    }
+},timeInterval);
